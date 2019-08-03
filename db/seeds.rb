557746ecfd23e365b2939924b50210d7e5b9cec8
Levi Miller <paullevimiller@gmail.com>
2019-08-02 17:46:02 -0700

    More representative seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,13 @@
-10.times{ User.create(name: Faker::Name.name) }
-10.times{ Host.create(name: Faker::Artist.name) }
-10.times{ Location.create(name: Faker::Restaurant.name) }
-10.times{ Label.create(name: Faker::Creature::Animal.name) }
+9.times{
+  new_user = User.create(name: Faker::Name.name) 
+  new_user.labels << Label.create(name: Faker::Creature::Animal.name)
+  new_user.labels << Label.create(name: Faker::Superhero.power.downcase)
+  new_user.labels << Label.create(name: Faker::Color.color_name.downcase)
+  new_user.labels << Label.create(name: Faker::Food.dish.downcase) 
+}
+
+3.times{ 
+  new_location = Location.create(name: Faker::Restaurant.name)
+  new_host = Host.create(name: Faker::Artist.name, location: new_location) 
+  4.times{ new_host.labels << Label.all[rand(0...Label.all.length)]}
+}
